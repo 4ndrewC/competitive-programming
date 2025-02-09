e8470c0f1e9d97f135e1d6d848599af011b6ec41
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2025-02-09 14:34:20 -0600

    Update solvetemplate.cpp

--- templates/solvetemplate.cpp
+++ templates/solvetemplate.cpp
@@ -9,6 +9,7 @@ using namespace __gnu_pbds;
 typedef long long ll;
 typedef __gnu_pbds::tree<ll, __gnu_pbds::null_type, less_equal<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 
+#define int long long
 #define pb push_back
 #define mp make_pair
 #define pi pair<int, int>
@@ -18,13 +19,29 @@ typedef __gnu_pbds::tree<ll, __gnu_pbds::null_type, less_equal<int>, __gnu_pbds:
 
 #define debug(lab) (cout<<"here "<<lab<<endl)
 
-int modinv(int a, int p){
-    return static_cast<int>(pow(a,p-2))%p;
+int gcdextended(int a, int b, int *x, int *y){
+    if(a == 0){
+        *x = 0, *y = 1;
+        return b;
+    }
+    int x1, y1;
+    int gcd = gcdextended(b%a, a, &x1, &y1);
+    *x = y1 - (b/a) * x1;
+    *y = x1;
+
+    return gcd;
 }
 
-int moddiv(int a, int b, int p){
-    int b_inv = modinv(b,p);
-    return (a*b_inv)%p;
+int modinv(int b, int m){
+    int x, y;
+    int g = gcdextended(b, m, &x, &y);
+    return (x%m + m) % m;
+}
+
+int moddiv(int a, int b, int m){
+    a = a % m;
+    int inv = modinv(b, m);
+    return (inv*a)%m;
 }
 
 void xen(){
@@ -32,7 +49,7 @@ void xen(){
 }
 
 
-int main(){
+int32_t main(){
     cin.tie(nullptr)->sync_with_stdio(false);
     int t; cin>>t;
     while(t--) xen();
